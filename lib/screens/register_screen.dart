9ba6724755360fba36cb9bb673ba27e8c5ce0522
Michael Lay <michaellay1912@yahoo.com>
2023-04-30 16:03:16 +0700

    page register done

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -11,45 +11,132 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController NameCtrl = TextEditingController();
+  final TextEditingController EmailCtrl = TextEditingController();
+  final TextEditingController PasswordCtrl = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              const Text('Form Register'),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-                    (states) {
-                      return primaryButton;
-                    },
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const ScreenLayout(),
+        child: Center(
+          child: Container(
+            width: 400,
+            child: Column(children: [
+              Image.asset(
+                "logo.png",
+                width: 300,
+              ),
+              const Text(
+                'REGISTER',
+                style: TextStyle(
+                    fontSize: 36, color: signText, fontWeight: FontWeight.bold),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, bottom: 3),
+                      child: const Text(
+                        "Name",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: signText,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  );
-                }, 
-                child: const Text('Register')
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-                    (states) {
-                      return secondaryButton;
-                    },
-                  ),
+                    TextField(
+                      controller: NameCtrl,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: inputField,
+                          contentPadding: EdgeInsets.only(left: 20),
+                          hintText: "Enter Name",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 10, bottom: 3, top: 10),
+                      child: const Text(
+                        "E-mail Address",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: signText,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    TextField(
+                      controller: EmailCtrl,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: inputField,
+                          contentPadding: EdgeInsets.only(left: 20),
+                          hintText: "Enter E-mail",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 10, bottom: 3, top: 10),
+                      child: const Text(
+                        "Password",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: signText,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    TextField(
+                      controller: PasswordCtrl,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: inputField,
+                          contentPadding: EdgeInsets.only(left: 20),
+                          hintText: "Enter Password",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                    ),
+                  ],
                 ),
-                onPressed: () {
-                  Navigator.pop(context);
-                }, 
-                child: const Text('Cancel')
               ),
-            ]
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 10),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        backgroundColor: primaryButton,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 120, vertical: 20)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      'REGISTER',
+                      style: TextStyle(fontSize: 20),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only( bottom: 10),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        backgroundColor: secondaryButton,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 130, vertical: 20)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      'CANCEL',
+                      style: TextStyle(fontSize: 20),
+                    )),
+              ),
+            ]),
           ),
         ),
       ),
